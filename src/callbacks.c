61ca0392cb2196b896a2eabc9e276c6b1f41e7d8
Rob <rob.manea@gmail.com>
2010-02-19 11:09:21 +0100

    added modifier mask to key_press/release events

--- src/callbacks.c
+++ src/callbacks.c
@@ -541,7 +541,7 @@ key_press_cb (GtkWidget* window, GdkEventKey* event) {
     (void) window;
 
     if(event->type == GDK_KEY_PRESS)
-        key_to_event(event->keyval, GDK_KEY_PRESS);
+        key_to_event(event->keyval, event->is_modifier?0:event->state, GDK_KEY_PRESS);
 
     return uzbl.behave.forward_keys ? FALSE : TRUE;
 }
@@ -551,7 +551,7 @@ key_release_cb (GtkWidget* window, GdkEventKey* event) {
     (void) window;
 
     if(event->type == GDK_KEY_RELEASE)
-        key_to_event(event->keyval, GDK_KEY_RELEASE);
+        key_to_event(event->keyval, event->is_modifier?0:event->state, GDK_KEY_RELEASE);
 
     return uzbl.behave.forward_keys ? FALSE : TRUE;
 }
--- src/events.h
+++ src/events.h
@@ -41,6 +41,6 @@ void
 send_event(int type, const gchar *details, const gchar *custom_event);
 
 void
-key_to_event(guint keyval, gint mode);
+key_to_event(guint keyval, guint state, int mode);
 
 #endif
