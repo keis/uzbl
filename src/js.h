04bed88785a4697d323c50c696f997f1806f9b23
David Keijser <keijser@gmail.com>
2016-03-01 08:40:16 +0100

    Split js command into smaller parts

--- src/js.h
+++ src/js.h
@@ -1,9 +1,14 @@
 #ifndef UZBL_JS_H
 #define UZBL_JS_H
 
-#include "webkit.h"
-
+#include <glib.h>
 #include <JavaScriptCore/JavaScript.h>
+
+typedef enum {
+    JSCTX_UZBL,
+    JSCTX_CLEAN,
+    JSCTX_PAGE
+} UzblJSContext;
 
 JSObjectRef
 uzbl_js_object (JSContextRef ctx, const gchar *prop);
@@ -15,5 +20,9 @@ gchar *
 uzbl_js_to_string (JSContextRef ctx, JSValueRef obj);
 gchar *
 uzbl_js_extract_string (JSStringRef str);
+gchar *
+uzbl_js_run_string (UzblJSContext context, const gchar *script);
+gchar *
+uzbl_js_run_file (UzblJSContext context, const gchar *path);
 
 #endif
